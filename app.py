4e7fa88f120522ff59b8af9f3263937b53068811
Pbbragg1 <Pbbragg1@boe.jack.k12.wv.us>
2024-10-08 11:30:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import game
 import phrase
+#this simply pulls everything together to call it and start the game, the dunder main insures that when it is imported to others it will not run
 if __name__ == "__main__":
     game.Game.start()
